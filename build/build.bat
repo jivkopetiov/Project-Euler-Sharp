00f4f5d72cc6987648fe0584dcac18e13833c9fd
Jivko Petiov <jivkopetiov@gmail.com>
2010-04-30 19:17:31 +0300

    migrate to .NET 4.0 RTM;
    fix nunit runner when there are spaces in the path to the source-code

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
 @echo off
-set MSBUILD="%WINDIR%/Microsoft.NET/Framework/v4.0.21006/msbuild.exe"
+set MSBUILD="%WINDIR%/Microsoft.NET/Framework/v4.0.30319/msbuild.exe"
 %MSBUILD% /nologo build.proj
 PAUSE
